5fd20dff5adaf9e6c2472304b5f032035153df21
Soumyaranjankalia <a2soumya.ranjan@gmail.com>
2025-10-02 15:35:37 +0530

    done

--- Task2.py
+++ Task2.py
@@ -1,4 +1,6 @@
 firstName=input('Enter your first name: ')
 secondName=input('Enter your second name: ')
 
-print('Hello ', firstName, secondName+'!','Welcome to the Python program') 
+full_name=firstName+" "+secondName
+
+print('Hello '+full_name+'! Welcome to the Python program') 
